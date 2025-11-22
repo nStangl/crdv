c5bd127eacd00a61f10d4fd574d31478ad361fb4
Niklas Stangl <67476141+nStangl@users.noreply.github.com>
2025-11-22 21:05:28 +0100

    Remove configurable maxHops parameter

--- reproducibility/containers/pg_crdv_entrypoint.sh
+++ reproducibility/containers/pg_crdv_entrypoint.sh
@@ -10,12 +10,11 @@ until pg_isready -h localhost -p 5432 > /dev/null 2>&1; do
 done
 
 SCHEMA_DIR="schema/sql"
-MAX_HOPS=${MAX_HOPS:-1}
 
 execute_sql_files() {
   local folder="$1"
   for file in $(find "$folder" -type f -name "*.sql" ! -name "00-drop.sql" | sort); do
-    sed "s/{{MAX_HOPS}}/$MAX_HOPS/g" "$file" | psql -q -v ON_ERROR_STOP=1 -U postgres -d testdb
+    psql -q -v ON_ERROR_STOP=1 -U postgres -d testdb -f "$file"
   done
 }
 
