ce08e6ff46a8824ef0aea548bb1510d622b0e663
Niklas Stangl <67476141+nStangl@users.noreply.github.com>
2025-11-22 19:43:30 +0100

    Install missing dependency

--- deploy/install_metrics_server.sh
+++ deploy/install_metrics_server.sh
@@ -2,7 +2,7 @@
 # Installs the hardware metrics server
 
 sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install python3 cron tcpdump iproute2 -y
+sudo DEBIAN_FRONTEND=noninteractive apt install python3 cron tcpdump iproute2 net-tools -y
 # run on startup
 (crontab -l 2>/dev/null; echo "@reboot sudo python3 $(pwd)/metrics_server.py > /tmp/metrics_server.log 2>&1") | crontab -
 # run now
